a19adef34b1a1241e86725e746e61633549e210a
Vineeth K Kolarath <flowinsight.app@gmail.com>
2026-06-18 14:10:10 +0530

    Fix PWA service worker caching

--- apps/doctor-flow/next.config.js
+++ apps/doctor-flow/next.config.js
@@ -2,6 +2,19 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig = {
